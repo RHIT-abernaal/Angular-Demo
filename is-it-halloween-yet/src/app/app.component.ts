8323de91fdbe6f5b2bd3228cdac56941594b09c2
Joseph Clark Parsons <43769180+rhit-parsonjc@users.noreply.github.com>
2022-10-24 19:48:03 -0400

    added Halloween image and text

--- is-it-halloween-yet/src/app/app.component.ts
+++ is-it-halloween-yet/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'is-it-halloween-yet';
+  title = 'Is it Halloween?';
+  today = new Date();
 }
