a6c087c53fde5083b8867d5ebcf3e12be91210cf
Joseph Clark Parsons <43769180+rhit-parsonjc@users.noreply.github.com>
2022-10-24 20:28:40 -0400

    I made a program quick and easy that I like

--- is-it-halloween-yet/src/app/app.component.ts
+++ is-it-halloween-yet/src/app/app.component.ts
@@ -11,7 +11,11 @@ export class AppComponent {
   monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   month = this.today.getMonth();
   day = this.today.getDate();
-  halloweenImage = "/assets/halloween-5596921_1280.jpg";
+  holidays = [
+    {month: 10, day: 31, image: "/assets/halloween-5596921_1280.jpg"},
+    {month: 1, day: 1, image: "/assets/happy-new-year-1063797_1280.jpg"},
+    {month: 7, day: 4, image: "/assets/eagle-1063863_1920.jpg"},
+  ];
   changeMonth(change: number) : void {
     this.month += change;
   }
